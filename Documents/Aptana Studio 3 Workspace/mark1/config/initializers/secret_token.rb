003c7042bf2f559a0359defd97bd3a486d314bda
metaputushoth <puru@metasofttechsolutions.com>
2013-04-27 16:40:50 -0400

    Initial commit

---
+++ Documents/Aptana Studio 3 Workspace/mark1/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mark1::Application.config.secret_token = '37d4b1d69909dc590b29f989f5044b956a42e9b8251ae0bb88cb2f0d6d85dc354b7be7d3c1e744143044aa5deecfa9c83d51e40d4eb25c5a8752e61ceb690a28'
